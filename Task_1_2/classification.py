9bc220b077559e4715765727c34cc91275a85981
LennardKnorke <janlennardgrabowski@gmx.de>
2024-05-18 09:33:13 +0200

    base set up?

---
+++ Task_1_2/classification.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+##############################################
+# CLASSES AND FUNCTIONS FOR THE SEGMENTATION SECTION
+##############################################
+class CNN_Network:
+    pass
+
+
+def save_hyperparameters():
+    pass
+
+
+
+
+
+
+##############################################
+# Main script to look for the best hyperparameters
+##############################################
+if __name__ == '__main__':
+    pass
--- Task_1_2/main.py
+++ Task_1_2/main.py
@@ -1,1 +1,19 @@
-# Temporary
+import numpy as np
+
+
+from Task_1_2.segmentation import segm_temp
+from Task_1_2.augmentation import aug_temp
+from Task_1_2.classification import CNN_Network, save_hyperparameters
+
+
+
+
+
+
+
+
+##############################################
+#
+##############################################
+if __name__ == '__main__':
+    pass
