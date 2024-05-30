315adff72866d11dff79a9b96331dbf2d3dd0c73
LennardKnorke <janlennardgrabowski@gmx.de>
2024-05-30 11:51:56 +0200

    CRNN progress. overall set up and network

--- Task_3/network.py
+++ Task_3/network.py
@@ -1,5 +1,135 @@
 import torch
 import torch.nn as nn
+from sklearn import preprocessing     
 
 class Recurrent_CNN(nn.Module):
-    pass
+    def __init__(self,
+                 num_classes: int,
+
+                 Conv_Channels : int = 32, conv_kern : int = 7, conv_stride : int = 1, conv_padd : int = 1,
+
+                 ResNet1_channels : int = 64, ResNet1_kern : int = 3, ResNet1_stride : int = 1, ResNet1_pad : int = 1,
+                 ResNet2_channels : int = 128, ResNet2_kern : int = 3, ResNet2_stride : int = 1, ResNet2_pad : int = 1,
+                 ResNet3_channels : int = 256, ResNet3_kern : int = 3, ResNet3_stride : int = 1, ResNet3_pad : int = 0,
+
+                 rnn_size_1 : int = 256, rnn_dropout_1 : float = 0.25,
+                 rnn_size_2 : int = 256, rnn_dropout_2 : float = 0.25,
+                 rnn_size_3 : int = 256, rnn_dropout_3 : float = 0.25
+                 ):
+        """
+        Initialize a RCNN model
+        @param input_size: size of the input tensor
+        @param num_classes: number of classes to predict
+        @param optimizer: torch optimizer to use for training
+        
+        @param cnn_channels_1: number of output channels in the first convolutional layer
+        @param cnn_kernel_1: size of the kernel in the first convolutional layer
+        @param cnn_stride_1: stride of the first convolutional layer
+        @param cnn_padding_1: padding of the first convolutional layer
+
+        @param rnn_size_1: number of neurons of the first recurrent layer
+        @param rnn_dropout_1: dropout rate of the first recurrent layer
+        """
+        super(Recurrent_CNN, self).__init__()
+        # Set up Convolutional Layers
+        self.Convolutional_Module = nn.Sequential(
+            nn.Conv2d(1, Conv_Channels, conv_kern, conv_stride, conv_padd),
+            nn.BatchNorm2d(Conv_Channels),
+            nn.ReLU(),
+
+            nn.MaxPool2d(2)
+        )
+
+        # First ResNet Block
+        self.ResNet_Module_1 = nn.Sequential(
+            nn.Conv2d(Conv_Channels, ResNet1_channels, ResNet1_kern, ResNet1_stride, ResNet1_pad),
+            nn.BatchNorm2d(ResNet1_channels),
+            nn.ReLU(),
+
+            nn.Conv2d(ResNet1_channels, ResNet1_channels, ResNet1_kern, ResNet1_stride, ResNet1_pad),
+            nn.BatchNorm2d(ResNet1_channels),
+            nn.ReLU(),
+
+            # Includes Max Pooling at the end
+            nn.MaxPool2d(2)
+        )
+
+        # Second ResNet Block
+        self.ResNet_Module_2 = nn.Sequential(
+            nn.Conv2d(ResNet1_channels, ResNet2_channels, ResNet2_kern, ResNet2_stride, ResNet2_pad),
+            nn.BatchNorm2d(ResNet2_channels),
+            nn.ReLU(),
+
+            nn.Conv2d(ResNet2_channels, ResNet2_channels, ResNet2_kern, ResNet2_stride, ResNet2_pad),
+            nn.BatchNorm2d(ResNet2_channels),
+            nn.ReLU(),
+
+            nn.Conv2d(ResNet2_channels, ResNet2_channels, ResNet2_kern, ResNet2_stride, ResNet2_pad),
+            nn.BatchNorm2d(ResNet2_channels),
+            nn.ReLU(),
+
+            nn.Conv2d(ResNet2_channels, ResNet2_channels, ResNet2_kern, ResNet2_stride, ResNet2_pad),
+            nn.BatchNorm2d(ResNet2_channels),
+            nn.ReLU(),
+
+            # Includes Max Pooling at the end
+            nn.MaxPool2d(2)
+        )
+
+        # Third ResNet Block
+        self.ResNet_Module_3 = nn.Sequential(
+            nn.Conv2d(ResNet2_channels, ResNet3_channels, ResNet3_kern, ResNet3_stride, ResNet3_pad),
+            nn.BatchNorm2d(ResNet3_channels),
+            nn.ReLU(),
+
+            nn.Conv2d(ResNet3_channels, ResNet3_channels, ResNet3_kern, ResNet3_stride, ResNet3_pad),
+            nn.BatchNorm2d(ResNet3_channels),
+            nn.ReLU(),
+
+            nn.Conv2d(ResNet3_channels, ResNet3_channels, ResNet3_kern, ResNet3_stride, ResNet3_pad),
+            nn.BatchNorm2d(ResNet3_channels),
+            nn.ReLU(),
+
+            nn.Conv2d(ResNet3_channels, ResNet3_channels, ResNet3_kern, ResNet3_stride, ResNet3_pad),
+            nn.BatchNorm2d(ResNet3_channels),
+            nn.ReLU()
+            # No max pooling at the end
+        )
+
+        self.ColumnPooling_Module = nn.AdaptiveAvgPool2d((1, None))
+
+        self.Recurrent_Module = nn.Sequential(
+            nn.LSTM(ResNet3_channels, rnn_size_1, 1, batch_first = True, dropout = rnn_dropout_1, bidirectional = True),
+            nn.LSTM(rnn_size_1*2, rnn_size_2, 1, batch_first = True, dropout = rnn_dropout_2, bidirectional = True),
+            nn.LSTM(rnn_size_2*2, rnn_size_3, 1, batch_first = True, dropout = rnn_dropout_3, bidirectional = True),
+
+            nn.LazyLinear(num_classes + 1)
+        )
+
+        self.CTC_Block = nn.Conv2d(ResNet3_channels, num_classes + 1, 3, 1, 1)
+
+    
+        return
+
+    def forward(self, image : torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass through the network
+        @param x: input tensor
+        @return: output tensor
+        """
+        bs, c, h, w = image.size()
+
+        x = self.Convolutional_Module(image)
+        x = self.ResNet_Module_1(x)
+        x = self.ResNet_Module_2(x)
+        x = self.ResNet_Module_3(x)
+        x = self.ColumnPooling_Module(x)
+        x = x.squeeze(2).permute(0, 2, 1)
+        print(x.shape)
+        print(type(x))
+
+        output_rnn, state_rnn = self.Recurrent_Module(x)
+        output_ctc = self.CTC_Block(x)
+
+        return output_rnn, output_ctc
+
--- Task_3/training.py
+++ Task_3/training.py
@@ -1,78 +1,291 @@
+# Import third party modules
 import cv2
-import numpy as np
-from network import *
+import itertools
 import matplotlib.pyplot as plt
-import torch
+import numpy as np
 import os
-import torch.nn as nn
-from torchinfo import summary
+from sklearn.model_selection import KFold, train_test_split
+from sklearn import metrics
+import torch
 import torch.optim as optim
-
+from torch.utils.data import DataLoader, Subset, Dataset
 from typing import Tuple
+
+# Our modules
+from network import *
+SEED = 42
+
+# Learning Makros
+BATCH_SIZE = 32
+N_EPOCHS = 240
+N_FOLDS = 10
+TEST_RATIO = 0.2
+IMAGE_WIDTH = 1024
+IMAGE_HEIGHT = 128
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def get_y_from_file(name : str, labelFile) -> str:
     """
-    Search for name in list and return line below it
-
+    Search for a name in list and return line below it.
+    @param name: name to search for
+    @param labelFile: list of labels
+    @return: the label of the name
     """
     for i, row in enumerate(labelFile):
         if name == row:
             return labelFile[i + 1]
     return ""
 
-def load_data(paths : list, file_names : list, labelFile)-> Tuple[np.ndarray, list, list, list]:
-    """
-    Create the x, y dataset as well as the file names
-    Load the images as x, and read the target sentence from the label file
-    @param paths: list of paths to the images
-    @param file_names: list of names of the images
-    @param labelFile: path to the label file
-    @return: the input, target and name lists
-    """
-    x = []
-    y = []
-    names = []
-    characters = []
-    for i in zip(paths, file_names):
-        # Get inputs as images
-        img = cv2.imread(i[0], cv2.IMREAD_GRAYSCALE)
-        img = cv2.resize(img, (1500, 80))
-        x.append(img)
 
-        # Get target sentences
-        label = get_y_from_file(i[1], labelFile)
-        y.append(label)
+class IAM_Dataset(Dataset):
+    def __init__(self, images_folder : str, labels_file):
+        """
+        Complete dataset to be used for training and testing
+        @param images_folder: folder containing the images
+        @param labels_file: file containing the labels
+        """
+        # Set up Image folder
+        self.images_folder = images_folder
+        self.images_files = os.listdir(images_folder)
+        self.images_paths = [images_folder + name for name in self.images_files]
+
+        # Read gt file with labels
+        with open(labels_file, "r") as f:
+            label_gt = f.readlines()
+        label_gt = [label.strip() for label in label_gt if label.strip()]
+
+        # Get target strings and all characters used
+        self.labels = []
+        self.chars = []
+        self.n_chars = 0
+        for file_name in self.images_files:
+            # Get target label (sentence)
+            label = get_y_from_file(file_name, label_gt)
+            assert label != "", "No label found for image " + file_name
+            label = " " + label + " "
+            self.labels.append(label)
+
+            # Get all characters that are used
+            for ch in label:
+                if ch not in self.chars:
+                    self.chars.append(ch)
+            # Update Max Length
+            if len(label) > self.n_chars:
+                self.n_chars = len(label)
         
-        for ch in label:
-            if ch not in characters:
-                characters.append(ch)
-        # 
-        names.append(i[1])
+        print("Number of images: ", len(self.images_files))
+        print("Max length of target string: ", self.n_chars)
+        print("Number of unique characters: ", len(self.chars))
+        print("Characters: ", self.chars)
+        return
 
-    return x, y, names, characters
+    def __len__(self):
+        return len(self.images_files)
+    
+    def __getitem__(self, idx):
+        img = cv2.imread(self.images_paths[idx], cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (IMAGE_WIDTH, IMAGE_HEIGHT))
+        img = img / 255.0
+        img = torch.tensor(img, dtype=torch.float32).unsqueeze(0)
+        
+        return img, self.labels[idx]
+    
+def strings_to_targets(strings : Tuple[str]) -> torch.Tensor:
+    """
+    Convert the a tuple with the full target strings into a tensor
+    @param strings: tuple of strings
+    @return: tensor with the targets
+    """
+    # Create a list of lists with each character as a target
+    targets  = [[char for char in full_string] for full_string in strings]
+    # Flatten
+    targets_flat = [char for sublist in targets for char in sublist]
+    encoder = preprocessing.LabelEncoder()
+    encoder.fit(targets_flat)
+    targets_enc = [encoder.transform(y) for y in targets]
+    #targets_enc = np.array(targets) + 1
+
+    print(np.unique(targets_enc))
+    print(targets_enc)
+    return targets_enc
+
+def train_model(model : nn.Module, 
+                training_data : Dataset,
+                training_loader : DataLoader,
+                testing_data : Dataset,
+                testing_loader : DataLoader,
+                optimizer : optim,
+                scheduler : optim.lr_scheduler
+                )-> list:
+    """
+    Train a model on a dataset and run validation with given optimization/scheduler
+    @param model: model to train
+    @param training_data: dataset to train on
+    @param training_loader: loader for the training data
+    @param validation_data: dataset to validate on
+    @param validation_loader: loader for the validation data
+    @param optimizer: optimizer to use for training
+    @param scheduler: scheduler to use for training
+    @return: history of the training and validation loss/accurracy as a list
+    """
+    history = []
+
+
+    for ep in range(N_EPOCHS):
+        loss_train = 0.0
+        acc_train = 0.0
+
+        # Training Loop
+        for images, full_label_strings in training_loader:
+            targets = strings_to_targets(full_label_strings)
+            optimizer.zero_grad()
+            model.train()
+
+            images = images.to(DEVICE)
+
+            # Forward Pass
+            pred = model(images)
+            print(pred)
+
+            # Backward Pass
+
+        
+        # Testing Loop
+        loss_test = 0.0
+        acc_test = 0.0
+        with torch.no_grad():
+            for images, labels in testing_loader:
+                model.eval()
+
+                images = images.to(DEVICE)
+                print(images.shape, labels)
+                # Forward Pass
+                pred = model(images)
+
+        # End of Epoch
+        history.append((
+            ep,
+            (loss_train, acc_train),
+            (loss_test, acc_test)
+        ))
+    return history
+
 # Training script to train the Recurrent_CNN model for task 2. outputs the model to a file as well as results
 if __name__ == "__main__":
     print("Training a Network for IAM Handwriting Database")
 
-    img_path = "./Task_3/img/"
-    img_names = os.listdir(img_path)
-    paths = [img_path + name for name in img_names]
 
-    label_gt = "./Task_3/iam_lines_gt.txt"
-    with open(label_gt, "r") as f:
-        label_gt = f.readlines()
-    label_gt = [label.strip() for label in label_gt if label.strip()]
+    # Prepapre data (training and testing data)
+    Data = IAM_Dataset(images_folder = "./Task_3/img/", 
+                       labels_file = "./Task_3/iam_lines_gt.txt"
+                       )
+    train_data, test_data = train_test_split(Data, test_size = TEST_RATIO, random_state = SEED)
+    print("Training data: ", len(train_data))
+    print("Testing data: ", len(test_data))
 
-    input_images, target_strings, file_names, all_chars = load_data(paths, img_names, label_gt)
 
-    # Models and Parameter
+    # Set up KFold
+    kFolds = KFold(n_splits = N_FOLDS, shuffle = True, random_state = SEED)
+    # Parameters to search for
+    parameters = {
+        "learning_rate": [0.001, 0.0001],
+        "beta": [(0.9, 0.999), (0.95, 0.999)],
+        "rnn_size": [128, 256],
+        "rnn_dropout": [0.25, 0.5]
+    }
+    paramList = list(itertools.product(*parameters.values()))
 
-    print(f"Length of input images: {len(input_images)}")
-    print(f"Length of target strings: {len(target_strings)}")
-    print(f"Length of file names: {len(file_names)}")
-    print(f"Total number of characters: {len(all_chars)}") 
-    print(f"Characters: {all_chars}")
-    print("\n")
+    best_loss = 1000000
+    best_params = []
+
+    # Train different models for multiple folds
+    for params in paramList:
+        print("Training with parameters: ", params)
+        learning_rate, beta, rnn_size, rnn_dropout = params
+
+        losses = []
+        for train_idx, test_idx in kFolds.split(train_data):
+            # Set up CRNN model + optimizer
+            model = Recurrent_CNN(Data.n_chars, 
+                                  rnn_size_1=rnn_size, rnn_dropout_1=rnn_dropout,
+                                  rnn_size_2=rnn_size, rnn_dropout_2=rnn_dropout,
+                                  rnn_size_3=rnn_size, rnn_dropout_3=rnn_dropout
+                                  )
+            model.to(DEVICE)
+
+            optimizer = optim.Adam(model.parameters(), lr=learning_rate, betas=beta)
+            scheduler = optim.lr_scheduler.StepLR(optimizer, step_size = 30, gamma = 0.1)
+
+            # Set up data for the current fold
+            train_data_fold = Subset(train_data, train_idx)
+            test_data_fold = Subset(train_data, test_idx)
+            train_loader = DataLoader(train_data_fold, batch_size = BATCH_SIZE, shuffle = True)
+            test_loader = DataLoader(test_data_fold, batch_size = BATCH_SIZE, shuffle = False)
+
+            #
+            history = train_model(model = model,
+                                  training_data = train_data_fold,
+                                  training_loader = train_loader,
+                                  testing_data = test_data_fold,
+                                  testing_loader = test_loader,
+                                  optimizer = optimizer,
+                                  scheduler = scheduler)
+            
+            # Remember last val loss of current fold
+            losses.append(history[-1][1][0])
+        # After all folds, check the mean loss and remember best params
+        mean_loss = np.mean(losses)
+        if mean_loss < best_loss:
+            best_loss = mean_loss
+            best_params = params
+            print("New best model found with parameters: ", params)
     
+    ##########################################################################################################
+    # Train the best model with all the training data and test on testing data
+    # Save the performance and model at the end
+    ##########################################################################################################
+    print("Best model found with parameters: ", best_params)
+    learning_rate, beta, rnn_size, rnn_dropout = best_params
+    model = Recurrent_CNN(Data.n_chars, 
+                          rnn_size_1=rnn_size, rnn_dropout_1=rnn_dropout,
+                          rnn_size_2=rnn_size, rnn_dropout_2=rnn_dropout,
+                          rnn_size_3=rnn_size, rnn_dropout_3=rnn_dropout)
+    model.to(DEVICE)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate, betas=beta)
 
+    train_loader = DataLoader(train_data, batch_size = BATCH_SIZE, shuffle = True)
+    test_loader = DataLoader(test_data, batch_size = BATCH_SIZE, shuffle = False)
+
+    history = train_model(model = model,
+                          training_data = train_data,
+                          training_loader = train_loader,
+                          validation_data = test_data,
+                          validation_loader = test_loader,
+                          optimizer = optimizer,
+                          scheduler = scheduler
+                          )
     
+    # Save the model
+    torch.save(model.state_dict(), "./Task_3/IAM_the_best_model.pth")
+    
+    # Plot performance
+    plt.figure()
+    plt.plot(history[0][0], label="Training Loss")
+    plt.plot(history[1][0], label="Testing Loss")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.title("Loss")
+    plt.savefig("./Task_3/loss_plot.png")
+    plt.close()
+
+    plt.figure()
+    plt.plot(history[1][1], label="Testing Accuracy")
+    plt.plot(history[0][1], label="Training Accuracy")
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.legend()
+    plt.title("Accuracy")
+    plt.savefig("./Task_3/accuracy_plot.png")
+    plt.close()
+    # Done
